97bd952436c2d8e6d12d1e1cab51e1375f0dcc03
prasankumar164 <seosolution076@gmail.com>
2021-09-21 22:16:12 +0530

    first commit

---
+++ js/home.js
@@ -0,0 +1,47 @@
+document.querySelector('.hamburger').addEventListener('click', (e) => {
+  e.preventDefault()
+
+  document.querySelector('.navigation').classList.toggle('active')
+})
+document.querySelector('.into').addEventListener('click', (e) => {
+  e.preventDefault()
+
+  document.querySelector('.navigation').classList.toggle('active')
+})
+
+document.querySelector('.btn_new_one').addEventListener('click', (e) => {
+  e.preventDefault()
+
+  document.querySelector('.bk_process').classList.toggle('actice_bk')
+})
+document.querySelector('.into_bk').addEventListener('click', (e) => {
+  e.preventDefault()
+
+  document.querySelector('.bk_process').classList.toggle('actice_bk')
+})
+
+// dark mode
+const toggleSwitch = document.querySelector(
+  '.theme-switch input[type="checkbox"]'
+)
+const currentTheme = localStorage.getItem('theme')
+
+if (currentTheme) {
+  document.documentElement.setAttribute('data-theme', currentTheme)
+
+  if (currentTheme === 'dark') {
+    toggleSwitch.checked = true
+  }
+}
+
+function switchTheme(e) {
+  if (e.target.checked) {
+    document.documentElement.setAttribute('data-theme', 'dark')
+    localStorage.setItem('theme', 'dark')
+  } else {
+    document.documentElement.setAttribute('data-theme', 'light')
+    localStorage.setItem('theme', 'light')
+  }
+}
+
+toggleSwitch.addEventListener('change', switchTheme, false)
